84302a95d194169b6ca16918d08bb80de26145da
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2023-01-26 13:45:24 +0530

    slug edited

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import Layout from 'components/Layout'
+import data from 'utils/data'
+import Link from 'next/link';
 
-export default function [slug]() {
+export default function ProductScreen() {
+  const {query} = useRouter();
+  const {slug} = query;
+  const product = data.products.find((x)=> x.slug === slug);
+  if(!product){
+    return <div>Product not found</div>;
+  }
   return (
-    <div>[slug]</div>
+    <Layout title={product.name}>
+      <div className='py-2 '>
+        <Link href="/">Back to products</Link>
+      </div>
+    </Layout>
   )
 }
